77e8161113dcc3c3c01964c3f55cf10655f43aa3
Chris <Chris@ultramediaburner.com>
2020-10-22 17:36:00 +0100

    first commit

---
+++ Mobile-VocGRE/src/main/java/org/ets/MobileVocGreApplication.java
@@ -0,0 +1,13 @@
+package org.ets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileVocGreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileVocGreApplication.class, args);
+	}
+
+}
